8728a225b22ede3c97b1b43e63b239e2630f44f7
Mindy Medeiros <mmedei6@wgu.edu>
2022-08-31 03:23:23 -0400

    Learning how to use math within simple print statements in Python

---
+++ math.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+# MATH!!
+
+print(50 + 50) # add
+print(50 - 50) # subtract
+print(50 * 50) # multiply
+print(50 / 50) # divide
+print(50 + 50 - 50 * 50 / 50) # PEMDAS
+print(50 ** 2) # exponents
+print(50 % 6) # modulo
+print(50 / 6) 
+print(50 // 6) # no leftovers
